0436dfd3c23233d19ea89f3d18811d5d2bcd077a
Gunpreet Singh <gunpreet@outlook.com>
2017-07-20 14:31:18 -0700

    update moment relation to profiles

--- db/models/moments.js
+++ db/models/moments.js
@@ -2,17 +2,10 @@ const db = require('../');
 
 const Moment = db.Model.extend({
   tableName: 'moments',
-  moments: function () {
-    return this.hasMany('Moment');
+  users() {
+    return this.hasMany('Profile');
   }
 });
 
+
 module.exports = db.model('Moment', Moment);
-
-
-// all moments given user id 
-// all info for a moment 
-// add moment to db 
-// update moment given uri 
-// update avg sentiment 
-
