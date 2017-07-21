20e4ac0894dca22b90c3788ab307873d659c585d
Gunpreet Singh <kllrjatt@users.noreply.github.com>
2017-07-21 10:45:34 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const config = require('config');
 
-const PORT = process.env.port || 3000;
+const PORT =  process.env.PORT || config.servers.madeleine.port;
 
 app.listen(PORT, () => {
   /* eslint-disable no-console */
