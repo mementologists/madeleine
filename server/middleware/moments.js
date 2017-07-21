54129382bcd5df3e14240cb9ed7a97ad48c02217
Gunpreet Singh <kllrjatt@users.noreply.github.com>
2017-07-21 15:28:59 -0700

    Update moments.js

--- server/middleware/moments.js
+++ server/middleware/moments.js
@@ -1,5 +1,5 @@
 const Axios = require('axios');
-const config = require('config').servers.services;
+// const config = require('config').servers.services;
 const models = require('../../db/models');
 const Promise = require('bluebird');
 const s3 = require('../lib').s3;
