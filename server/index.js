56211082fcc542a5f6a7fe33c99b6ff96a328ce3
Gunpreet Singh <gunpreet@outlook.com>
2017-07-25 15:47:10 -0700

    add docker file config, docker comose and docker ignore files.  add outside comm for server.

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ const app = require('./app');
 const config = require('config');
 
 const PORT = process.env.PORT || config.servers.madeleine.port;
+const HOST = '0.0.0.0'; // outside access
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, '', () => {
   /* eslint-disable no-console */
   console.log(`madeleine server listening on port ${PORT}!`);
   /* eslint-enable no-console */
